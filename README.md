6684116e699ea6b275531bacdfb97d582e1fa2ad
Tylor Allison <tylor@foobox.com>
2024-01-29 06:03:36 -0600

    build stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gizmo
+
+A vanilla JS gamedev library.
+
+** WIP documentation **
---
+++ indexer.js
@@ -0,0 +1,17 @@
+const glob = require("glob");
+const fs = require('fs');
+
+let contents = "";
+
+glob("js/**/*.js", (error, filesWithJs) => {
+    if (error) {
+        console.log(error);
+    }
+    for (const file of filesWithJs) {
+        if (file === 'js/index.js') continue;
+        if (file === 'js/main.js') continue;
+        contents += `export * from './${file.slice(3)}'\n`;
+    }
+    fs.writeFileSync("js/index.js", contents);
+    
+});
