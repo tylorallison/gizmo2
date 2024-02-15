86d5e1733c6d990a763cb2d07cd3151644e906fd
Tylor Allison <tylor@foobox.com>
2024-02-14 18:07:45 -0600

    templates

--- js/templates.js
+++ js/templates.js
@@ -7,16 +7,18 @@ import { SketchMixer } from './sketchMixer.js';
 
 class SheetTemplate extends Gadget {
     static { this.$schema('mediaDir', { dflt:'media' }); }
-    static { this.$schema('width', { dflt:16 }); }
-    static { this.$schema('height', { dflt:16 }); }
+    static { this.$schema('width', { dflt:32 }); }
+    static { this.$schema('height', { dflt:32 }); }
     static { this.$schema('xoff', { dflt:0 }); }
     static { this.$schema('yoff', { dflt:0 }); }
 
-    spriteFromIJ(tag, src, ij) {
+    spriteFromIJ(tag, src, ij, overrides={}) {
+        let width = overrides.width || this.width;
+        let height = overrides.width || this.height;
         let media = ImageMedia.xspec({
             src:`${this.mediaDir}/${src}.png`, 
-            width:this.width,
-            height:this.height,
+            width:width,
+            height:height,
             x:this.xoff + this.width*ij.x, 
             y:this.yoff + this.height*ij.y,
         });
@@ -27,13 +29,13 @@ class SheetTemplate extends Gadget {
         return asset;
     }
 
-    mixerFromIJs(tag, src, ijs) {
+    mixerFromIJs(tag, src, ijs, overrides={}) {
         let variations = [];
         let i = 0;
         for (const ij of ijs) {
             i++;
             let vtag = `${tag}.v${i}`;
-            variations.push(this.spriteFromIJ(vtag, src, ij));
+            variations.push(this.spriteFromIJ(vtag, src, ij, overrides));
         }
         return SketchMixer.xspec({ 
             tag:tag,
@@ -42,38 +44,74 @@ class SheetTemplate extends Gadget {
     }
 }
 
+/**
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ * |   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ * | 0 |   |   |   |ctl| t | t |ctr|   |   |   |
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ * | 1 |   |   |   | l |   |   | r |   |   |   |
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ * | 2 |   |   |ctl|jtl|       |jtr|ctr|   |   |
+ * +---+---+---+---+---+   m   +---+---+---+---+
+ * | 3 |ctl| t |jtl|   |       |   |jtr| t |ctr|
+ * +---+---+---+---+---+-------+---+---+---+---+
+ * | 4 | l |   |       |       |       | m | r |
+ * +---+---+---+   m   |   m   |   m   +---+---+
+ * | 5 | l |   |       |       |       | m | r |
+ * +---+---+---+---+---+-------+---+---+---+---+
+ * | 6 |cbl| b |jbl|   |       |   |jbr| b |cbr|
+ * +---+---+---+---+---+   m   +---+---+---+---+
+ * | 7 |   |   |cbl|jbl|       |jbr|cbr|   |   |
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ * | 8 |   |   |   | l |   |   | r |   |   |   |
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ * | 9 |   |   |   |cbl| b | b |cbr|   |   |   |
+ * +---+---+---+---+---+---+---+---+---+---+---+
+ */
 class AutotilerMap extends Gadget {
     static {
-        this.$schema('ctl',     { dflt: [{x:0, y:0}] });
-        this.$schema('t',       { dflt: [{x:1, y:0}] });
-        this.$schema('ctr',     { dflt: [{x:2, y:0}] });
-        this.$schema('l',       { dflt: [{x:0, y:1}] });
-        this.$schema('m',       { dflt: [{x:1, y:1}] });
-        this.$schema('r',       { dflt: [{x:2, y:1}] });
-        this.$schema('cbl',     { dflt: [{x:0, y:2}] });
-        this.$schema('b',       { dflt: [{x:1, y:2}] });
-        this.$schema('cbr',     { dflt: [{x:2, y:2}] });
-        this.$schema('jbr',     { dflt: [{x:3, y:0}] });
-        this.$schema('jbl',     { dflt: [{x:5, y:0}] });
-        this.$schema('jtr',     { dflt: [{x:3, y:2}] });
-        this.$schema('jtl',     { dflt: [{x:5, y:2}] });
+        this.$schema('base',    { dflt: [{x:4,y:2}, {x:2,y:4}, {x:4,y:4}, {x:6,y:4}, {x:4,y:6}] });
+        this.$schema('ctl',     { dflt: [{x:3,y:0}, {x:2,y:2}, {x:0,y:3}] });
+        this.$schema('t',       { dflt: [{x:4,y:0}, {x:5,y:0}, {x:1,y:3}, {x:8,y:3}] });
+        this.$schema('ctr',     { dflt: [{x:6,y:0}, {x:7,y:2}, {x:9,y:3}] });
+        this.$schema('r',       { dflt: [{x:6,y:1}, {x:9,y:4}, {x:9,y:4}, {x:9,y:5}] });
+        this.$schema('jtr',     { dflt: [{x:6,y:2}, {x:7,y:3}] });
+        this.$schema('cbr',     { dflt: [{x:9,y:6}, {x:7,y:7}, {x:6,y:9}] });
+        this.$schema('b',       { dflt: [{x:1,y:6}, {x:8,y:6}, {x:4,y:9}, {x:5,y:9}] });
+        this.$schema('jbr',     { dflt: [{x:7,y:6}, {x:6,y:7}] });
+        this.$schema('cbl',     { dflt: [{x:0,y:6}, {x:2,y:7}, {x:3,y:9}] });
+        this.$schema('l',       { dflt: [{x:3,y:1}, {x:0,y:4}, {x:0,y:5}, {x:3,y:8}] });
+        this.$schema('jbl',     { dflt: [{x:2,y:6}, {x:3,y:7}] });
+        this.$schema('jtl',     { dflt: [{x:3,y:2}, {x:2,y:3}] });
     }
 
     entries() {
-        return Object.entries(this.$store);
+        return Object.entries(this);
     }
 }
 
 class AutotilerTemplate extends SheetTemplate {
+    static { this.$schema('width', { dflt:16 }); }
+    static { this.$schema('height', { dflt:16 }); }
     static { this.$schema('map',     { dflt: () => new AutotilerMap() }); }
+
     static from(src, tag, spec={}) {
+        let tmp = new this(spec);
+        return tmp.sketches(src, tag);
+    }
+
+    sketches(src, tag) {
         if (!tag) tag = src;
         let xspecs = [];
-        let tmp = new this(spec);
-        for (const [which,ijs] of tmp.map.entries()) {
+        // push spec for base sketch
+        xspecs.push(this.mixerFromIJs(tag, src, this.map.base, {width:this.width*2, height:this.height*2}));
+        // push specs for each of the "sides" of the autotiler map
+        for (const [which,ijs] of this.map.entries()) {
             let mtag = `${tag}.${which}`;
-            xspecs.push(SheetTemplate.mixerFromIJs(mtag, src, ijs));
+            xspecs.push(this.mixerFromIJs(mtag, src, ijs));
         }
         return xspecs;
     }
+
 }
--- test/ttemplates.js
+++ test/ttemplates.js
@@ -1,5 +1,5 @@
 import { Fmt } from '../js/fmt.js';
-import { SheetTemplate } from '../js/templates.js';
+import { AutotilerTemplate, SheetTemplate } from '../js/templates.js';
 
 describe('sheet templates', () => {
 
@@ -10,10 +10,10 @@ describe('sheet templates', () => {
         let x = tmp.spriteFromIJ('tsprite', 'file', {x:1,y:2});
         expect(x.args[0].tag).toEqual('tsprite');
         expect(x.args[0].media.args[0].src).toEqual('test/file.png');
-        expect(x.args[0].media.args[0].x).toEqual(16);
-        expect(x.args[0].media.args[0].y).toEqual(32);
-        expect(x.args[0].media.args[0].width).toEqual(16);
-        expect(x.args[0].media.args[0].height).toEqual(16);
+        expect(x.args[0].media.args[0].x).toEqual(32);
+        expect(x.args[0].media.args[0].y).toEqual(64);
+        expect(x.args[0].media.args[0].width).toEqual(32);
+        expect(x.args[0].media.args[0].height).toEqual(32);
     });
 
     it('can load a sprite mixer', ()=>{
@@ -25,12 +25,21 @@ describe('sheet templates', () => {
         expect(x.args[0].tag).toEqual('tmixer');
         expect(x.args[0].variations[0].args[0].tag).toEqual('tmixer.v1');
         expect(x.args[0].variations[0].args[0].media.args[0].src).toEqual('test/file.png');
-        expect(x.args[0].variations[0].args[0].media.args[0].x).toEqual(16);
-        expect(x.args[0].variations[0].args[0].media.args[0].y).toEqual(32);
+        expect(x.args[0].variations[0].args[0].media.args[0].x).toEqual(32);
+        expect(x.args[0].variations[0].args[0].media.args[0].y).toEqual(64);
         expect(x.args[0].variations[1].args[0].tag).toEqual('tmixer.v2');
         expect(x.args[0].variations[1].args[0].media.args[0].src).toEqual('test/file.png');
-        expect(x.args[0].variations[1].args[0].media.args[0].x).toEqual(32);
-        expect(x.args[0].variations[1].args[0].media.args[0].y).toEqual(48);
+        expect(x.args[0].variations[1].args[0].media.args[0].x).toEqual(64);
+        expect(x.args[0].variations[1].args[0].media.args[0].y).toEqual(96);
     });
 
+});
+
+describe('autotiler templates', () => {
+    it('can generate default specs', ()=>{
+        let tmp = new AutotilerTemplate();
+        let sketches = tmp.sketches('autofile', 'autotest');
+        console.log(`sketches: ${sketches}`);
+        expect(sketches.find((v) => v.args[0].tag === 'autotest')).toBeTruthy();
+    });
 });
