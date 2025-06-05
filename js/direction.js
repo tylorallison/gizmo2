526680e4c27f8d0bf42350178fdf1fa2a52e7fd8
Tylor Allison <tylor@foobox.com>
2025-06-05 09:55:00 -0500

    wip:support grid3array

--- js/direction.js
+++ js/direction.js
@@ -2,14 +2,34 @@ export { Direction };
 
 class Direction {
     static none =           0;
-    static north =          1;
-    static northEast =      2;
-    static east =           4;
-    static southEast =      8;
-    static south =          16;
-    static southWest =      32;
-    static west =           64;
-    static northWest =      128;
+    static north =          1<<0;
+    static east =           1<<1;
+    static south =          1<<2;
+    static west =           1<<3;
+    static northEast =      1<<4;
+    static southEast =      1<<5;
+    static northWest =      1<<6;
+    static southWest =      1<<7;
+    static up =             1<<8;
+    static down =           1<<9;
+
+    static northUp =        1<<10;
+    static eastUp =         1<<11;
+    static southUp =        1<<12;
+    static westUp =         1<<13;
+    static northEastUp =    1<<14;
+    static southEastUp =    1<<15;
+    static northWestUp =    1<<16;
+    static southWestUp =    1<<17;
+    static northDown =      1<<18;
+    static eastDown =       1<<19;
+    static southDown =      1<<20;
+    static westDown =       1<<21;
+    static northEastDown =  1<<22;
+    static southEastDown =  1<<23;
+    static northWestDown =  1<<24;
+    static southWestDown =  1<<25;
+
     static cardinal =       Direction.north|Direction.south|Direction.east|Direction.west;
     static diagonal =       Direction.northWest|Direction.southWest|Direction.northEast|Direction.southEast;
     static any =            Direction.cardinal|Direction.diagonal;
@@ -23,7 +43,66 @@ class Direction {
         this.south,
         this.southWest,
         this.west,
-    ]
+    ];
+
+    static all3d = [
+        this.northWest,
+        this.north,
+        this.northEast,
+        this.east,
+        this.southEast,
+        this.south,
+        this.southWest,
+        this.west,
+        this.up,
+        this.down,
+        this.northWestUp,
+        this.northUp,
+        this.northEastUp,
+        this.eastUp,
+        this.southEastUp,
+        this.southUp,
+        this.southWestUp,
+        this.westUp,
+        this.northWestDown,
+        this.northDown,
+        this.northEastDown,
+        this.eastDown,
+        this.southEastDown,
+        this.southDown,
+        this.southWestDown,
+        this.westDown,
+    ];
+
+    static opposites = {
+        [this.northWest]:       this.southEast,
+        [this.north]:           this.south,
+        [this.northEast]:       this.southWest,
+        [this.east]:            this.west,
+        [this.southEast]:       this.northWest,
+        [this.south]:           this.north,
+        [this.southWest]:       this.northEast,
+        [this.west]:            this.east,
+        [this.up]:              this.down,
+        [this.down]:            this.up,
+        [this.northWestUp]:     this.southEastDown,
+        [this.northUp]:         this.southDown,
+        [this.northEastUp]:     this.southWestDown,
+        [this.eastUp]:          this.westDown,
+        [this.southEastUp]:     this.northWestDown,
+        [this.southUp]:         this.northDown,
+        [this.southWestUp]:     this.northEastDown,
+        [this.westUp]:          this.eastDown,
+        [this.northWestDown]:   this.southEastUp,
+        [this.northDown]:       this.southUp,
+        [this.northEastDown]:   this.southWestUp,
+        [this.eastDown]:        this.westUp,
+        [this.southEastDown]:   this.northWestUp,
+        [this.southDown]:       this.northUp,
+        [this.southWestDown]:   this.northEastUp,
+        [this.westDown]:        this.eastUp,
+    };
+
     static strMap = {
         [this.north]:       'north',
         [this.northEast]:   'northEast',
@@ -33,6 +112,24 @@ class Direction {
         [this.southWest]:   'southWest',
         [this.west]:        'west',
         [this.northWest]:   'northWest',
+        [this.up]:          'up',
+        [this.down]:        'down',
+        [this.northUp]:     'northUp',
+        [this.northEastUp]: 'northEastUp',
+        [this.eastUp]:      'eastUp',
+        [this.southEastUp]: 'southEastUp',
+        [this.southUp]:     'southUp',
+        [this.southWestUp]: 'southWestUp',
+        [this.westUp]:      'westUp',
+        [this.northWestUp]: 'northWestUp',
+        [this.northDown]:     'northDown',
+        [this.northEastDown]: 'northEastDown',
+        [this.eastDown]:      'eastDown',
+        [this.southEastDown]: 'southEastDown',
+        [this.southDown]:     'southDown',
+        [this.southWestDown]: 'southWestDown',
+        [this.westDown]:      'westDown',
+        [this.northWestDown]: 'northWestDown',
     };
     static abbrevMap = {
         [this.north]:       'n',
@@ -43,6 +140,24 @@ class Direction {
         [this.southWest]:   'sw',
         [this.west]:        'w',
         [this.northWest]:   'nw',
+        [this.up]:          'u',
+        [this.down]:        'd',
+        [this.northUp]:     'nu',
+        [this.northEastUp]: 'neu',
+        [this.eastUp]:      'eu',
+        [this.southEastUp]: 'seu',
+        [this.southUp]:     'su',
+        [this.southWestUp]: 'swu',
+        [this.westUp]:      'wu',
+        [this.northWestUp]: 'nwu',
+        [this.northDown]:       'nd',
+        [this.northEastDown]:   'ned',
+        [this.eastDown]:        'ed',
+        [this.southEastDown]:   'sed',
+        [this.southDown]:       'sd',
+        [this.southWestDown]:   'swd',
+        [this.westDown]:        'wd',
+        [this.northWestDown]:   'nwd',
     };
 
     static cardinals = [
@@ -52,11 +167,35 @@ class Direction {
         this.west,
     ];
 
+    static cardinals3d = [
+        this.north,
+        this.east,
+        this.south,
+        this.west,
+        this.up,
+        this.down,
+    ];
+
     static diagonals = [
         this.northEast,
         this.southEast,
         this.southWest,
         this.northWest,
+    ];
+
+    static diagonals3d = [
+        this.northEast,
+        this.southEast,
+        this.southWest,
+        this.northWest,
+        this.northEastUp,
+        this.southEastUp,
+        this.southWestUp,
+        this.northWestUp,
+        this.northEastDown,
+        this.southEastDown,
+        this.southWestDown,
+        this.northWestDown,
     ];
 
     static composites = {
@@ -158,10 +297,22 @@ class Direction {
         case Direction.west:
         case Direction.northWest:
         case Direction.southWest:
+        case Direction.westUp:
+        case Direction.northWestUp:
+        case Direction.southWestUp:
+        case Direction.westDown:
+        case Direction.northWestDown:
+        case Direction.southWestDown:
             return -1;
         case Direction.east:
         case Direction.northEast:
         case Direction.southEast:
+        case Direction.eastUp:
+        case Direction.northEastUp:
+        case Direction.southEastUp:
+        case Direction.eastDown:
+        case Direction.northEastDown:
+        case Direction.southEastDown:
             return 1;
         }
         return 0;
@@ -181,26 +332,58 @@ class Direction {
         return 0;
     }
 
-    static opposite(dir) {
+    static asY(dir) {
         switch(dir) {
         case Direction.north:
-            return Direction.south;
-        case Direction.south:
-            return Direction.north;
-        case Direction.west:
-            return Direction.east;
-        case Direction.east:
-            return Direction.west;
         case Direction.northWest:
-            return Direction.southEast;
-        case Direction.southEast:
-            return Direction.northWest;
         case Direction.northEast:
-            return Direction.southWest;
+        case Direction.northUp:
+        case Direction.northWestUp:
+        case Direction.northEastUp:
+        case Direction.northDown:
+        case Direction.northWestDown:
+        case Direction.northEastDown:
+            return -1;
+        case Direction.south:
         case Direction.southWest:
-            return Direction.northEast;
+        case Direction.southEast:
+        case Direction.southUp:
+        case Direction.southWestUp:
+        case Direction.southEastUp:
+        case Direction.southDown:
+        case Direction.southWestDown:
+        case Direction.southEastDown:
+            return 1;
         }
-        return Direction.none;
+        return 0;
+    }
+
+    static asZ(dir) {
+        switch(dir) {
+        case Direction.northUp:
+        case Direction.northEastUp:
+        case Direction.eastUp:
+        case Direction.southEastUp:
+        case Direction.southUp:
+        case Direction.southWestUp:
+        case Direction.westUp:
+        case Direction.northWestUp:
+            return 1;
+        case Direction.northDown:
+        case Direction.northEastDown:
+        case Direction.eastDown:
+        case Direction.southEastDown:
+        case Direction.southDown:
+        case Direction.southWestDown:
+        case Direction.westDown:
+        case Direction.northWestDown:
+            return -1;
+        }
+        return 0;
+    }
+
+    static opposite(dir) {
+        return this.opposites[dir] || 0;
     }
 
     static orthogonal(dir) {
